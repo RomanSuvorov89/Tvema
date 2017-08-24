36d0e341e971c3fb8c15f10f623f39bca90c8a9d
Roman <wd3rer@gmail.com>
2017-08-24 21:57:41 +0300

    Реализация класса

--- Tvema/FixedThreadPool.cs
+++ Tvema/FixedThreadPool.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 
 namespace Tvema
 {
-    class FixedThreadPool
+    class FixedThreadPool 
     {
         public ITask Task;
 
+        #region public enum Priority
         public enum Priority
         {
             Low = 0,
@@ -16,23 +17,42 @@ namespace Tvema
             High = 2
         };
 
+        #endregion
+        #region public FixedThreadPool
         public FixedThreadPool(int threadCount)
         {
             
         }
+        #endregion
+        #region public bool Execute
         public bool Execute(Task task, Priority priority)
         {
 
         }
-
+        #endregion
+        #region public void Stop
         public void Stop()
         {
 
         }
+        #endregion
+
+        #region InnerInterface
+
+        interface InnerInterface
+        {
+            bool Execute(Task task, Priority priority);
+            void Stop();
+        }
+
+        #endregion
     }
 
+    #region interface ITask
     interface ITask
     {
         void Execute();
     }
+    #endregion
+
 }
