8051fcd3bf7211db6827ac4f80e5c969da9eeb9a
Roman <wd3rer@gmail.com>
2017-08-24 16:10:30 +0300

    start

---
+++ Tvema/FixedThreadPool.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tvema
+{
+    class FixedThreadPool
+    {
+        public ITask Task;
+
+        public enum Priority
+        {
+            Low = 0,
+            Normal = 1,
+            High = 2
+        };
+
+        public FixedThreadPool(int threadCount)
+        {
+            
+        }
+        public bool Execute(Task task, Priority priority)
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+
+    interface ITask
+    {
+        void Execute();
+    }
+}
